e5bfc492e1ad4cec609b1fb8588e2857c41c3c3d
anlan <anlan.lhy@gmail.com>
2022-04-08 23:24:26 +0800

    文件

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,3 +1,14 @@
-export const  constant =  {
+let judegeSec = () => {
+    let ishttps = 'https:' == document.location.protocol ? true: false;
+    var url = window.location.host;
+      if(ishttps){
+        return true;
+   }else{
+        return false;
+   }
+}
+
+export const constant = {
+    // baseURL: judegeSec()? 'https://anlan.club':'http://anlan.club',
     baseURL: 'http://127.0.0.1:8080',
 };
--- src/components/tools/file/index.jsx
+++ src/components/tools/file/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Upload, message, Row, Col, } from 'antd';
+import { Upload, message, Row, Col, notification } from 'antd';
 import { CloudUploadOutlined } from '@ant-design/icons';
 import service from "../../../api/http";
 import { constant } from '../../../api/constant';
@@ -7,6 +7,7 @@ import MyFileList from "./list"
 
 const { Dragger } = Upload;
 
+const key = 'UPLOAD_PERCENT';
 
 class MyUpLoadFile extends React.Component {
 
@@ -28,6 +29,7 @@ class MyUpLoadFile extends React.Component {
             total: 1,
             current: 1
         },
+        percent: 0,
         pageSize: 10,
     }
 
@@ -70,18 +72,106 @@ class MyUpLoadFile extends React.Component {
     };
 
     onFileChange = (info) => {
+        // 上传状态
         const { status } = info.file;
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
+        // 上传进度
+        const { percent } = info.file;
+        console.log(`${parseFloat(percent.toFixed(2))}%`);
+        this.setState({
+            percent: percent,
+        })
+        if (status === 'uploading') {
+            notification.open({
+                key,
+                message: `【${info.file.name}】正在上传中`,
+                description: `上传进度【${parseFloat(percent.toFixed(2))}%】`,
+                duration: 10,
+            });
         }
         if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
+            notification.open({
+                key,
+                message: `【${info.file.name}】正在上传成功`,
+                description: `上传进度【${parseFloat(percent.toFixed(2))}%】`,
+                duration: 3,
+            });
             this.getList(1, 10);
         } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
+            notification.open({
+                key,
+                message: `【${info.file.name}】正在上传失败 `,
+                description: `上传进度【${parseFloat(percent.toFixed(2))}%】`,
+                duration: 3,
+            });
         }
-        
+
     }
+
+    uuid = () => {
+        var s = [];
+        var hexDigits = "0123456789abcdef";
+        for (var i = 0; i < 36; i++) {
+            s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+        }
+        s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+        s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+        s[8] = s[13] = s[18] = s[23] = "-";
+     
+        var uuid = s.join("");
+        return uuid;
+    }
+
+
+
+    customBigReq = (item) => {
+        const size = item.file.size;
+        const chunkSize = 1024 * 4;
+        const maxChunk = Math.ceil(size / chunkSize);
+        var uuid = this.uuid();
+        for (var index = 0; index < maxChunk; index++) {
+            const start = index * chunkSize;
+            let end = start + chunkSize;
+            if (size - end < 0) {
+                end = size;
+            }
+            let file = item.file.slice(start, end);
+            const formData = new FormData();
+            // var arrayObj = new Array();
+            // arrayObj.push(file);
+            // let chunkFile = {
+            //     'files': arrayObj,
+            //     'chunkSize': file.size,
+            //     'fileSize': size,
+            //     'fileName': size,
+            //     'fileMd5':  this.uuid(),
+            //     'curIndex': index,
+            //     'allIndex': maxChunk
+            // }
+            formData.append('files',file);
+            formData.append('chunkSize',file.size);
+            formData.append('fileSize',size);
+            formData.append('fileName',item.file.name);
+            formData.append('fileMd5',uuid);
+            formData.append('curIndex',index);
+            formData.append('allIndex',maxChunk);
+            let url = constant.baseURL + '/basic/file/uploadChunkFile';
+            fetch(url, {
+                method: 'post',
+                body: formData,
+                credentials: 'include'
+            }).then((response) => {
+                message.success(`上传测试.`);
+            }).catch((error) => {
+                message.error(`上传测试失败.`);
+            });
+            this.sleep(3000);
+        }
+    }
+
+    sleep = function(time) {
+        var startTime = new Date().getTime() + parseInt(time, 10);
+        while(new Date().getTime() < startTime) {}
+    };
 
     render() {
 
@@ -91,7 +181,8 @@ class MyUpLoadFile extends React.Component {
             multiple: false,
             action: constant.baseURL + '/basic/file/uploadFile',
             showUploadList: false,
-            getList: this.getList
+            getList: this.getList,
+            customRequest: this.customBigReq
         };
 
         let fileProps = {
